5984550f3a48e41d0ebee557cc174781126ffb99
DKSHFIORI <137484938+DKSHFIORI@users.noreply.github.com>
2024-10-03 06:59:07 +0800

    Init Upload
    Init Upload

---
+++ dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"],(function(e){"use strict";return e.extend("dksh.ymm.mcfupdate.mcfupdate.Component",{metadata:{manifest:"json"}})}));
---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("dksh.ymm.mcfupdate.mcfupdate.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
